dd9c235341e04edc67ac043436f7899a334d5924
James Espinosa <de.james013@gmail.com>
2023-03-05 13:49:22 +0800

    removed heroku

--- d_api/settings.py
+++ d_api/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,5 +143,3 @@ CORS_ALLOWED_ORIGINS = [
     "https://airsick-lowlander.netlify.app"
     "https://thaidakar03.netlify.app"
 ]
-
-django_heroku.settings(locals(  ))
