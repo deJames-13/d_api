2ee3abb6b9dcb8e535fc5e9cccd8e687c6480520
James Espinosa <de.james013@gmail.com>
2023-03-05 14:05:36 +0800

    fixed origins

--- d_api/settings.py
+++ d_api/settings.py
@@ -140,6 +140,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://airsick-lowlander.netlify.app"
-    "https://thaidakar03.netlify.app"
+    "https://airsick-lowlander.netlify.app",
+    "https://thaidakar03.netlify.app",
 ]
