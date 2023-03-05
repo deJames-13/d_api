e7b6f9f8234146e0515fdfd8b755365956c00b38
James Espinosa <de.james013@gmail.com>
2023-03-05 14:43:47 +0800

    migrated new database

--- d_api/settings.py
+++ d_api/settings.py
@@ -90,8 +90,14 @@ WSGI_APPLICATION = 'd_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'CfEjhNZSJV61vLgtrjeg',
+        'HOST': 'containers-us-west-134.railway.app',
+        'PORT': '8078',
     }
 }
 
